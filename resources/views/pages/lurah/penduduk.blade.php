2bb1547bb8b223f3e3d1ceda0eb94228bf6cfbf1
wikukarno <prasetyagama2@gmail.com>
2022-10-10 10:46:32 +0700

    update title

--- resources/views/pages/lurah/penduduk.blade.php
+++ resources/views/pages/lurah/penduduk.blade.php
@@ -11,7 +11,7 @@ Data Penduduk
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Data Penduduk</h3>
+                        <h3 class="card-title">Data Pengguna</h3>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
