b3d13705918b7dc1496c3203993c95977688596d
wikukarno <prasetyagama2@gmail.com>
2022-11-04 00:54:20 +0700

    udpate

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pembaruan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <section class="section-maintenance-content mt-5">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-12 col-md-12">
+                    <figure class="figure">
+                        <img src="{{ asset('assets/images/maintenance.png') }}" class="figure-img img-fluid w-75 h-75"
+                            alt="Maintenance">
+                    </figure>
+                </div>
+                <div class="col-10 col-md-12">
+                    <div class="maintenance-content">
+                        <h1>Pembaruan Sistem!</h1>
+                        <p>
+                            Kami sedang melakukan pembaruan sistem, <br />
+                            Silahkan coba beberapa saat lagi.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
