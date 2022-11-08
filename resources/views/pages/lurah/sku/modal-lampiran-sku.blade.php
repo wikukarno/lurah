498c8c1e695035e1557a301701bf6cf9b9e9741a
wikukarno <prasetyagama2@gmail.com>
2022-11-08 11:05:04 +0700

    tambah modal lampiran

---
+++ resources/views/pages/lurah/sku/modal-lampiran-sku.blade.php
@@ -0,0 +1,131 @@
+<!-- Modal -->
+<div class="modal fade" id="lampiranSkuModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+	aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Data Pemohon</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">NIK</label>
+								<input type="number" class="form-control" id="sku_no_nik" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Nama</label>
+								<input type="text" class="form-control" id="sku_nama" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Nama Usaha</label>
+								<input type="text" class="form-control" id="sku_nama_usaha" readonly>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-12 col-lg-6 mb-sm-3">
+							<div class="form-group">
+								<label for="nama">Tempat Lahir</label>
+								<input type="text" class="form-control" id="sku_tempat_lahir" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-6 mb-sm-3">
+							<div class="form-group">
+								<label for="tanggal_lahir">Tanggal Lahir</label>
+								<input type="text" class="form-control" id="sku_tanggal_lahir" readonly>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Jenis Kelamin</label>
+								<input type="text" id="sku_jenis_kelamin" class="form-control" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Pekerjaan</label>
+								<input type="text" class="form-control" id="sku_pekerjaan" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Status Perkawinan</label>
+								<input type="text" class="form-control" id="sku_status_perkawinan" readonly>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Agama</label>
+								<input type="text" id="sku_agama" class="form-control" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Kecamatan</label>
+								<input type="text" class="form-control" id="sku_kecamatan" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-4">
+							<div class="form-group">
+								<label for="nama">Kelurahan</label>
+								<input type="text" class="form-control" id="sku_kelurahan" readonly>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-12 col-lg-6 mb-sm-3">
+							<div class="form-group">
+								<label for="nama">RT / RW</label>
+								<input type="text" class="form-control" id="sku_rt_rw" readonly>
+							</div>
+						</div>
+						<div class="col-12 col-lg-6 mb-sm-3">
+							<div class="form-group">
+								<label for="alamat">Alamat</label>
+								<input type="text" class="form-control" id="sku_alamat" readonly>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-12 col-lg-6 mb-3">
+							<div class="ktp">
+								<img alt="foto ktp" class="img-fluid" id="ktp">
+							</div>
+						</div>
+						<div class="col-12 col-lg-6 mb-3">
+							<div class="ktp">
+								<img alt="foto kk" class="img-fluid" id="kk">
+							</div>
+						</div>
+					</div>
+					<div class="row mt-2 text-center">
+						<div class="col-12">
+							<div class="rt_rw">
+								<img alt="foto surat rt/rw" class="img-fluid" id="surat_rt_rw">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
