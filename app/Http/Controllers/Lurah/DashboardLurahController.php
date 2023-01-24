16d823038bc3b33f4051a316a22a5e225a2494a3
wikukarno <prasetyagama2@gmail.com>
2023-01-24 11:21:51 +0700

    Perbarui

--- app/Http/Controllers/Lurah/DashboardLurahController.php
+++ app/Http/Controllers/Lurah/DashboardLurahController.php
@@ -108,7 +108,9 @@ class DashboardLurahController extends Controller
     public function getPenduduk()
     {
         if (request()->ajax()) {
-            $query = User::with('userDetails')->where('roles', 'User')->where('roles', 'Staff')->where('status_account', 'verifikasi')->get();
+            $query = User::with('userDetails')->where('roles', [
+                'User', 'Staff'
+            ])->where('status_account', 'verifikasi')->get();
 
             return datatables()->of($query)
                 ->addIndexColumn()
