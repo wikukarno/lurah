d4932fd71fea21da42219f7cbb9b61e050d1bab8
wikukarno <prasetyagama2@gmail.com>
2023-02-24 11:08:20 +0700

    update

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -42,7 +42,7 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
 <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
