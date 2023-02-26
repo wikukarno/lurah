8f31d1db8330e751d8561cd0ec8fc36e196bf3f5
wikukarno <prasetyagama2@gmail.com>
2023-02-26 22:10:40 +0700

    add pavicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('assets/images/logo.png') }}" type="image/x-icon" />
     <title>Lurah Sorek Satu</title>
 
     <!-- style -->
